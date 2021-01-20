d3c311f9873c35ad825eb98ce21147457ecd661b
Eyal Betzalel <eyalbatzul@gmail.com>
2021-01-20 14:17:23 +0200

    Create LoadData.py

---
+++ LoadData.py
@@ -0,0 +1,22 @@
+#############################################################################################################
+
+# Written by Eyal Betzalel 20.1.2021
+
+#############################################################################################################
+
+# In this section I will load generated dataset with NLL values and return unified table for all of them 
+
+# Inputs :  
+# 1. ImageGPT file path
+# 2. PixelSnail file path
+
+# Output : 
+#1. Unified table for all results
+
+#############################################################################################################
+
+# Import ImageGPT files : 
+
+# Import PixelSnail files:
+
+# Create unified table : 
