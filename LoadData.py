907f3ccb52ad50a81f3c893164a1c8f8f347c2a2
Eyal Betzalel <eyalbatzul@gmail.com>
2021-01-20 14:36:22 +0200

    Update LoadData.py

--- LoadData.py
+++ LoadData.py
@@ -9,6 +9,7 @@
 # Inputs :  
 # 1. ImageGPT file path
 # 2. PixelSnail file path
+# 3. same_model_compare_flag - if true two path are in the same format and belong to same model
 
 # Output : 
 #1. Unified table for all results
