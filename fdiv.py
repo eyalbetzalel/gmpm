91d793db4f8449483f9766d79aaf11fa388ab372
Eyal Betzalel <eyalbatzul@gmail.com>
2021-01-20 14:40:26 +0200

    Create fdiv.py

---
+++ fdiv.py
@@ -0,0 +1,17 @@
+#############################################################################################################
+
+# Written by Eyal Betzalel 20.1.2021
+
+#############################################################################################################
+
+# In this section I will analyze samples with different divergence functions : 
+
+# Inputs :
+# 1. Unified table with dataset and nll score for each input from different models
+# 2. --f (kl / js / ot / more?)
+
+# Output : 
+#1. score
+
+#############################################################################################################
+
